f6e1f76d367490b08feea22a7229b0a07e25bea1
EthanUphoff <ethan.uphoff@gmail.com>
2018-11-27 11:26:47 -0600

    Created UDPPacket class
    UDPPacket contains the status, FileID, packet number, and data from a packet.

---
+++ src/segmentedfilesystem/UDPPacket.java
@@ -0,0 +1,22 @@
+package segmentedfilesystem;
+
+import java.net.DatagramPacket;
+import java.util.ArrayList;
+
+public class UDPPacket {
+    private byte status;
+    private byte FileID;
+    private byte[] packetNumber = new byte[2];
+    private ArrayList<Byte> data;
+
+    public UDPPacket(DatagramPacket p){
+        byte[] s = p.getData();
+        this.status = s[0];
+        this.FileID = s[1];
+        this.packetNumber[0] = s[2];
+        this.packetNumber[1] = s[3];
+        for(int i = 4; i < p.getLength(); i++){
+            this.data.add(s[i]);
+        }
+    }
+}
