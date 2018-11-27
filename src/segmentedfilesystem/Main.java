b47862f01001f5c3c90268e7c9ff09c800fe33c2
EthanUphoff <ethan.uphoff@gmail.com>
2018-11-27 11:25:25 -0600

    Set up Main for DatagramSockets

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,30 @@
 package segmentedfilesystem;
 
+import java.io.*;
+import java.net.*;
+
 public class Main {
-    
-    public static void main(String[] args) {
-        
+    public static final int PORT_NUMBER = 6014;
+
+    public static void main(String[] args) throws IOException {
+        Main client = new Main();
+        client.start();
+    }
+
+    private void start() throws IOException {
+        InetAddress heartofgold = InetAddress.getByName("heartofgold.morris.umn.edu");
+        SocketAddress s = new InetSocketAddress(heartofgold, PORT_NUMBER);
+        DatagramSocket socket = new DatagramSocket();
+        socket.connect(s);
+        byte[] buf = new byte[10];
+        DatagramPacket p = new DatagramPacket(buf, 10);
+        socket.send(p);
+        socket.receive(p);
+            System.out.println(p.getData()[0]);
+            System.out.println(p.getData()[1]);
+            System.out.println(p.getData()[2]);
+            System.out.println(p.getData()[3]);
+            System.out.println(p.getData()[4]);
     }
 
 }
