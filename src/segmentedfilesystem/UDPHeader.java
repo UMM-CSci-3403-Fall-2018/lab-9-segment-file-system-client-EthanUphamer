09dcba04e3039f3dd33cbde1bb873f736cb8f0e0
EthanUphoff <ethan.uphoff@gmail.com>
2018-11-27 11:27:57 -0600

    Created UDPHeader class
    UDPHeader contains the status, FileID, and the file name of the header packet.

---
+++ src/segmentedfilesystem/UDPHeader.java
@@ -0,0 +1,19 @@
+package segmentedfilesystem;
+
+import java.net.DatagramPacket;
+import java.util.ArrayList;
+
+public class UDPHeader {
+    private byte status;
+    private byte FileID;
+    private ArrayList<Byte> filename;
+
+    public UDPHeader(DatagramPacket p){
+        byte[] s = p.getData();
+        this.status = s[0];
+        this.FileID = s[1];
+        for(int i = 2; i < p.getLength(); i++){
+            this.filename.add(s[i]);
+        }
+    }
+}
