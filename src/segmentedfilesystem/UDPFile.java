c666a4056f4e88301f29cc378791ab56ad3f29a9
EthanUphoff <ethan.uphoff@gmail.com>
2018-12-04 11:30:14 -0600

    Properly update UDPFile status and packet information.
    UDPFile now adds packets and headers accordingly and checks the completion status each time.

--- src/segmentedfilesystem/UDPFile.java
+++ src/segmentedfilesystem/UDPFile.java
@@ -1,12 +1,12 @@
 package segmentedfilesystem;
 
-import java.net.DatagramPacket;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class UDPFile {
     private boolean finished = false;
     private int length = Integer.MAX_VALUE;
-    private ArrayList<UDPPacket> packets;
+    private ArrayList<UDPPacket> packets = new ArrayList<UDPPacket>();
     private UDPHeader header;
     private byte FileID;
 
@@ -16,18 +16,38 @@ public class UDPFile {
     }
 
     public UDPFile(UDPPacket p){
-
+        addPacketToFile(p);
+        this.FileID = p.getFileID();
     }
 
     public void addHeaderToFile(UDPHeader header) {
       this.header = header;
+      if(length == this.packets.size()){
+          this.finished = true;
+      }
     }
 
     public void addPacketToFile(UDPPacket packet) {
       this.packets.add(packet);
+      if(isFinalPacket(packet)) {
+          this.length = packet.getPacketNumber();
+      }
+      if(this.header != null && this.length == this.packets.size()){
+          this.finished = true;
+          Collections.sort(this.packets);
+      }
     }
 
     public byte getFileID(){
       return this.FileID;
+    }
+
+    public boolean sendStatus(){return this.finished;}
+
+    private boolean isFinalPacket(UDPPacket p) {
+        if(p.getStatusByte() % 4 == 3) {
+            return true;
+        }
+        return false;
     }
 }
