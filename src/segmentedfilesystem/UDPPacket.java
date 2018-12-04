48b23c6fa58d84d9d47e0d41b9a8c3b1246f0870
EthanUphoff <ethan.uphoff@gmail.com>
2018-12-04 11:32:13 -0600

    Made packetNumber an int and added functions to get private data within UDPPacket.

--- src/segmentedfilesystem/UDPPacket.java
+++ src/segmentedfilesystem/UDPPacket.java
@@ -2,19 +2,20 @@ package segmentedfilesystem;
 
 import java.net.DatagramPacket;
 import java.util.ArrayList;
+import java.util.Comparator;
 
-public class UDPPacket {
+public class UDPPacket implements Comparable<UDPPacket> {
     private byte status;
     private byte FileID;
-    private byte[] packetNumber = new byte[2];
-    private ArrayList<Byte> data;
+    private int packetNumber;
+    private ArrayList<Byte> data = new ArrayList<Byte>();
 
     public UDPPacket(DatagramPacket p){
         byte[] s = p.getData();
         this.status = s[0];
         this.FileID = s[1];
-        this.packetNumber[0] = s[2];
-        this.packetNumber[1] = s[3];
+        this.packetNumber = (s[2] << 8) | s[3] & 0xFF;
+        System.out.println(this.packetNumber);
         for(int i = 4; i < p.getLength(); i++){
             this.data.add(s[i]);
         }
@@ -23,4 +24,11 @@ public class UDPPacket {
     public byte getFileID(){
       return this.FileID;
     }
+
+    public byte getStatusByte(){ return this.status;}
+
+    public int compareTo(UDPPacket p) { return this.getPacketNumber() - p.getPacketNumber(); }
+
+    public int getPacketNumber() { return this.packetNumber;}
+
 }
